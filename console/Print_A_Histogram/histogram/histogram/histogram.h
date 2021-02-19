dc46fd39b41f94ccae9b2f833bc5c7b0c0dab039
ZainUlMustafa <zain2984@gmail.com>
2021-02-19 22:29:16 +0500

    updates

--- console/Print_A_Histogram/histogram/histogram/histogram.h
+++
@@ -1,7 +0,0 @@
-#ifndef __HISTOGRAM_H__
-#define __HISTOGRAM_H__
-
-char array_of_alpha[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-int frequency(int* alpha);
-
-#endif
